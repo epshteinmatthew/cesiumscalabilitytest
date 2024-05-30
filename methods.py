ebac8ce90f4c830f846872d6df2df6ee30dd2c5e
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-05-29 23:22:31 -0700

    lookup operations
    they work but are quite slow on large ranges of data. I think the approach of distributing each model to its own file may be flawed.

--- methods.py
+++ methods.py
@@ -1,1 +1,13 @@
+import requests
+URL = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/metadata.json"
 
+def lookup(index):
+    data = requests.get(url=URL).json()
+    return requests.get(url = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/" + data[index.__str__()]).content
+
+def lookup_range(lower, upper):
+    data = requests.get(url=URL).json()
+    list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
+    return [ requests.get(url = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/" + item).content for item in list]
+
+print(lookup_range(123, 1240))
