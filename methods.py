0a5d42824fc74950c7294394b4ea7c0d76473855
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-05-30 23:05:55 -0700

    fix marge

--- methods.py
+++ methods.py
@@ -10,4 +10,3 @@ def lookup_range(lower, upper):
     data = json.loads(open("metadata.json", "r").read())
     list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
     return [  open(os.getcwd() + "/" + item).read() for item in list]
-
