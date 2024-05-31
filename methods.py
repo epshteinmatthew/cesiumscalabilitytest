5c2c02ca9cf01d79d903495a06fa43ec3380a57a
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-05-30 23:04:50 -0700

    lookup methods now use filesystem rather than requests api - this is MUCH faster

--- methods.py
+++ methods.py
@@ -1,13 +1,13 @@
-import requests
-URL = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/metadata.json"
+import json
+import os
+
 
 def lookup(index):
-    data = requests.get(url=URL).json()
-    return requests.get(url = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/" + data[index.__str__()]).content
+    data = json.loads(open("metadata.json", "r").read())
+    return open(os.getcwd() + "/" + data[index.__str__()]).read()
 
 def lookup_range(lower, upper):
-    data = requests.get(url=URL).json()
+    data = json.loads(open("metadata.json", "r").read())
     list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
-    return [ requests.get(url = "https://raw.githubusercontent.com/epshteinmatthew/cesiumscalabilitytest/main/" + item).content for item in list]
+    return [  open(os.getcwd() + "/" + item).read() for item in list]
 
-print(lookup_range(123, 1240))
