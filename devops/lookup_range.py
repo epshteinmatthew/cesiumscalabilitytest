5f5d3ffa1db5f091381a88703cb306ff6b4767f8
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-06-02 14:58:00 -0700

    Update lookup_range.py

--- devops/lookup_range.py
+++ devops/lookup_range.py
@@ -1,8 +1,10 @@
 import os, json
+import sys
+
 
 def lookup_range(lower, upper):
     data = json.loads(open("metadata.json", "r").read())
     list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
     return [  open(os.getcwd() + "/" + item).read() for item in list]
 
-print(lookup_range(100,12000))
+print(lookup_range(sys.argv[1],sys.argv[1]))
