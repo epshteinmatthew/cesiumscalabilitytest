850b423441cdfe0204a1f320b690beeb13e64c7f
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-06-02 15:10:34 -0700

    Update lookup_range.py

--- devops/lookup_range.py
+++ devops/lookup_range.py
@@ -7,4 +7,4 @@ def lookup_range(lower, upper):
     list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
     return [  open(os.getcwd() + "/" + item).read() for item in list]
 
-print(lookup_range(int(sys.argv[1]),int(sys.argv[1])))
+print(lookup_range(int(sys.argv[0]),int(sys.argv[1])))
