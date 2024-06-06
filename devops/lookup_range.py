fe27cb7d15246b0c7ad691c972defab9a06253ea
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-06-06 11:50:33 -0700

    hopefully this makes it faster?

--- devops/lookup_range.py
+++ devops/lookup_range.py
@@ -5,6 +5,19 @@ import sys
 def lookup_range(lower, upper):
     data = json.loads(open("metadata.json", "r").read())
     list = [item[1] for item in data.items() if int(item[0]) <= upper and int(item[0]) >= lower]
-    return [  open(os.getcwd() + "/" + item).read() for item in list]
+    output = "files="
+    for item in list:
+        output += item
+        output += "\n"
+    print(output)
 
-print(lookup_range(int(sys.argv[1]),int(sys.argv[2])))
+
+
+if sys.argv[3] == "1":
+    (lookup_range(int(sys.argv[1]),int(sys.argv[2])))
+else:
+    filenames = sys.argv[4].splitlines()
+    print([open(item, "r").read() for item in filenames])
+
+
+
